3463b027fa736167fd66d9dc9df5d75240fe2bc2
Gach Peter <gachpeter2@gmail.com>
2022-06-06 22:43:29 +0300

    corrected database

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/sample2", { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+mongoose.connect("mongodb://z5cf3cf17-mongodb.z21686b2e.qovery.cloud:27017/sample1", { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
     if (err) throw err;
     else console.log("db succeded");
 })
