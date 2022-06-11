f7469236dccd06b00fc239d842d94fe2aecf3741
gachpeter2@gmail.com <67166454+GachPeter@users.noreply.github.com>
2022-06-11 17:57:29 +0300

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 mongoose = require("mongoose");
 
-mongoose.connect( process.env.link, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+mongoose.connect( process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
     if (err) throw err;
     else console.log("db succeded");
 })
