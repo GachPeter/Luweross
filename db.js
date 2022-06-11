87990e900ff01c8379996e37885ea7ff0a7c1b36
gachpeter2@gmail.com <67166454+GachPeter@users.noreply.github.com>
2022-06-11 04:16:18 +0300

    Luwero ss survey db

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://survey_ss:005116011@cluster0.esvvm.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+mongoose.connect("mongodb+srv://survey_ss:005116011@cluster0.esvvm.mongodb.net/?retryWrites=true&w=majority/Luwero_survey", { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
     if (err) throw err;
     else console.log("db succeded");
 })
