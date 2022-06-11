b95347e756c13446ef79a4e606b830bdb1fc43fd
gachpeter2@gmail.com <67166454+GachPeter@users.noreply.github.com>
2022-06-11 04:06:09 +0300

    user scheme to feature

--- user.js
+++ user.js
@@ -2,6 +2,6 @@ const mongoose = require("mongoose");
 
 UserScheme = mongoose.Schema({ time: Number, name: String, feature: String, class: String, stream: String });
 
-User = mongoose.model("User", UserScheme);
+User = mongoose.model("Feature", UserScheme);
 
 module.exports = User;
