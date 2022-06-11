32d6551909669741b98bae4f854c7dc4acb3b5e8
gachpeter2@gmail.com <67166454+GachPeter@users.noreply.github.com>
2022-06-11 03:32:17 +0300

    port change

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const { urlencoded, request } = require("express");
 var app = express();
 var server = http.createServer(app);
 app.use(express.urlencoded({ extended: false }));
+var port = process.env.PORT || 3000;
 
 app.get("/feature", (req, res) => {
     User.find({}, (docs, err) => {
@@ -67,6 +68,6 @@ app.post("/feature", (req, res) => {
 
 module.exports = app;
 
-app.listen(3000, () => {
-    console.log(`Server started on port : 3000`);
+app.listen(port, () => {
+    console.log(`Server started on port : ${port}`);
 })
