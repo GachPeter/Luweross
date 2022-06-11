e7d10e7bcef7a59847305d87d1858b47321a2062
gachpeter2@gmail.com <67166454+GachPeter@users.noreply.github.com>
2022-06-11 04:02:43 +0300

    db link

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 mongoose = require("mongoose");
 
-mongoose.connect("mongodb://z5cf3cf17-mongodb.z21686b2e.qovery.cloud:27017/sample1", { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+mongoose.connect("mongodb+srv://survey_ss:005116011@cluster0.esvvm.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
     if (err) throw err;
     else console.log("db succeded");
 })
