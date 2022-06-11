11a11926898970c2713380fbbb7a21038ae58090
gachpeter2@gmail.com <67166454+GachPeter@users.noreply.github.com>
2022-06-11 17:50:43 +0300

    link update

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://survey_ss:005116011@cluster0.esvvm.mongodb.net/?retryWrites=true&w=majority/ls", { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+mongoose.connect( process.env.link, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
     if (err) throw err;
     else console.log("db succeded");
 })
