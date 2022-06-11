e93ecdf9a47f4f5b048e72f2c223fb2fd3a3f320
gachpeter2@gmail.com <67166454+GachPeter@users.noreply.github.com>
2022-06-11 17:42:44 +0300

    color to grey

--- html/js/script.js
+++ html/js/script.js
@@ -17,7 +17,7 @@ function loadit() {
     $.get("/feature", function (data) {
         let fn = data.reverse();
         fn.forEach(el => {
-            let m = "<div class='card mt-2' title='A feature' style='min-width:20em;'> <div class='card-body' > " + el.name + "<h4 class='card-title'><small style='float: right;'>" + "S" + el.class + " " + el.stream + "</small></h4> <p class='card-text text-warning'>FEATURE: " + el.feature + "<small style='position : absolute;right:10px;bottom:10px'><b>" + toFt(new Date(el.time).getHours()) + ":" + new Date(el.time).getMinutes() + " " + toDt(new Date(el.time).getHours())+"</b> " +new Date(el.time).getUTCDate() + "/" + new Date(el.time).getMonth()  +"/" +new Date(el.time).getFullYear() + "</small></p></div ></div >";
+            let m = "<div class='card mt-2' title='A feature' style='min-width:20em;'> <div class='card-body' > " + el.name + "<h4 class='card-title'><small style='float: right;'>" + "S" + el.class + " " + el.stream + "</small></h4> <p class='card-text text-warning'>FEATURE: " + el.feature + "<small style='position : absolute;right:10px;bottom:10px;color:grey'><b>" + toFt(new Date(el.time).getHours()) + ":" + new Date(el.time).getMinutes() + " " + toDt(new Date(el.time).getHours())+"</b> " +new Date(el.time).getUTCDate() + "/" + new Date(el.time).getMonth()  +"/" +new Date(el.time).getFullYear() + "</small></p></div ></div >";
             mother.innerHTML += m;
         });
     });
